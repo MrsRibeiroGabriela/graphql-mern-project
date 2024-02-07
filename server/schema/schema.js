2adcc6430024935ee103b7990d12ec04ef70899b
Gabriela Ribeiro <mrsribeirogabriela@MacBook-Pro-de-Gabriela.local>
2024-02-07 14:02:57 -0300

    Server: GraphQL queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,9 @@
 const { projects, clients } = require("../sampleData.js");
 
+//mongoose models
+const Project = require("../models/Project");
+const Client = require("../models/Client");
+
 const {
   GraphQLObjectType,
   GraphQLID,
@@ -20,9 +24,9 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find(client => client.id === parent.clientId);
-      }
-    }
+        return Client.findById(parent.clientId);
+      },
+    },
   }),
 });
 
@@ -44,27 +48,27 @@ const RootQuery = new GraphQLObjectType({
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        return Project.find();
       },
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return projects.find((project) => project.id === args.id);
+        return Project.findById(args.id);
       },
     },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients;
+        return Client.find();
       },
     },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id);
+        return Client.findBiId(args.id);
       },
     },
   },

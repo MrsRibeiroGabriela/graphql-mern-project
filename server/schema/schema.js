95b1f6db9329bdaa4c531aafb1d5efec02b22c28
Gabriela Ribeiro <mrsribeirogabriela@MacBook-Pro-de-Gabriela.local>
2024-02-15 20:41:54 -0300

    Fix: Cascade project delete when client delete

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -104,8 +104,11 @@ const mutation = new GraphQLObjectType({
       },
       resolve(parent, args) {
         Project.find({ clientId: args.id }).then((projects) => {
-          projects.forEach((project) => project.remove());
+          projects.forEach((project) => {
+            project.deleteOne();
+          });
         });
+
         return Client.findByIdAndDelete(args.id);
       },
     },
